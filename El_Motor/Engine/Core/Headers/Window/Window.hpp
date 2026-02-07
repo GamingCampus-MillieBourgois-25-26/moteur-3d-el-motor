5076e97d5a579f5de837ed422921a14816da9835
Maxime Luhat <133891284+Et3Aracno@users.noreply.github.com>
2026-02-07 10:26:02 +0100

    Amadéo Fix
    Fix all Cmake and include Glfs in Engine to enable window creation

--- El_Motor/Engine/Core/Headers/Window/Window.hpp
+++ El_Motor/Engine/Core/Headers/Window/Window.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <iostream>
-//#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <memory>
 
 class WindowOpener {
 private :
 	const int WIDTH = 800, HEIGHT = 600;
-	//std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window{ nullptr, glfwDestroyWindow };
+	std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window{ nullptr, glfwDestroyWindow };
 
 public:
 
 	void WindowDisplay();
 	void WindowInit();
 	void WindowRenderer();
-	//GLFWwindow* getMyWindow() { return window.get(); }
+	GLFWwindow* getMyWindow() { return window.get(); }
 
 	void test();
 };
