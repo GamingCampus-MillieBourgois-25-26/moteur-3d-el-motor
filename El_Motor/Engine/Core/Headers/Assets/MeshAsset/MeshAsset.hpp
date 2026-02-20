5b506497c692935c2fa4c148f27c9a2b987cf2cf
Axel Guionnet <64863449+GGNatio@users.noreply.github.com>
2026-02-20 21:46:31 +0100

    push rendering

--- El_Motor/Engine/Core/Headers/Assets/MeshAsset/MeshAsset.hpp
+++ El_Motor/Engine/Core/Headers/Assets/MeshAsset/MeshAsset.hpp
@@ -4,23 +4,37 @@
 #include <string>
 #include <d3d11.h>
 #include <DirectXMath.h>
+
 #include "Assets/Asset.hpp"
 
-struct Vertex {
+struct Vertex
+{
     DirectX::XMFLOAT3 position;
     DirectX::XMFLOAT3 normal;
     DirectX::XMFLOAT2 uv;
 };
 
-class MeshAsset : public Asset {
+class MeshAsset : public Asset
+{
 public:
+
     std::vector<Vertex> vertices;
     std::vector<uint32_t> indices;
 
     ID3D11Buffer* vertexBuffer = nullptr;
     ID3D11Buffer* indexBuffer = nullptr;
 
-    void Load() override;                     
-    void CreateBuffers(ID3D11Device* device); 
+    void Load() override;
     void Unload() override;
+
+    
+
+    void CreateBuffers(ID3D11Device* device);
+
+    void Bind(ID3D11DeviceContext* context);
+
+    void Draw(ID3D11DeviceContext* context);
+
+    bool IsReady() const;
+    UINT GetIndexCount() const;
 };
