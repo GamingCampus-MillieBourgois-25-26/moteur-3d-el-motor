31867cd0910f28abaa072dbee51c8196983de920
SpenzaYT <spenzayt@gmail.com>
2026-02-27 10:05:48 +0100

    fix again mesh component

--- El_Motor/Engine/Core/Headers/Entity/Component/MeshComponent.hpp
+++ El_Motor/Engine/Core/Headers/Entity/Component/MeshComponent.hpp
@@ -3,29 +3,30 @@
 #include "Entity/Component.hpp"
 #include "Asset_Manager/AssetManager.hpp"
 #include "Assets/MeshAsset/MeshAsset.hpp"
-#include <string>
+#include <memory>
 
 namespace Engine {
 
     class MeshComponent : public Component {
     private:
-        MeshAsset* m_mesh = nullptr;
+        std::shared_ptr<MeshAsset> m_mesh;
 
     public:
         MeshComponent() = default;
 
-        MeshAsset* GetMesh() const { return m_mesh; }
+        std::shared_ptr<MeshAsset> GetMesh() const { return m_mesh; }
 
         void LoadMesh(const std::string& path) {
-            auto assetPtr = AssetManager::Get().Load<MeshAsset>(path);
-            m_mesh = assetPtr.get();
+            m_mesh = AssetManager::Get().Load<MeshAsset>(path);
         }
 
-        void SetMesh(MeshAsset* mesh) { m_mesh = mesh; }
+        void SetMesh(std::shared_ptr<MeshAsset> mesh) {
+            m_mesh = mesh;
+        }
 
         void Start() override {}
         void Update(float dt) override {}
-        void OnDestroy() override { m_mesh = nullptr; }
+        void OnDestroy() override { m_mesh.reset(); }
 
         std::string GetTypeName() const override { return "MeshRenderer"; }
     };
